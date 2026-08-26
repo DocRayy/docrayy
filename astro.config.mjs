97bca01cdd9a0b6521d70729008c39f0a1b7d61b
Rayyan Kheisar <rayyyksr11@gmail.com>
2026-08-26 12:02:35 +0700

    fix performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://docrayy.vercel.app",
+  site: "https://docrayy.my.id",
   vite: {
     plugins: [tailwindcss()],
   },
